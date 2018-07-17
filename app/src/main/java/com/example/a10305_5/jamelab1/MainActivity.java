33b3b0e0bd9b4cf9b5f4765b2840f039f811abdc
10305-5 <a10305-5@iMac-7.local>
2018-07-17 14:13:19 +0700

    Star App

---
+++ app/src/main/java/com/example/a10305_5/jamelab1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a10305_5.jamelab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//ประกาศตัวแปร
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //เขียนโค้ด
+
+
+    }// end onCreate
+}// end main class
